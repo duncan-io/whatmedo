dbccde2b228cedfcac867bd17c002a2a6e1e32eb
duncan <dmckee95@gmail.com>
2022-11-28 17:02:08 -0500

    Add ability to switch lists and have lists populate in main area

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 import toDoList from "./displayList";
-import newTask from "./newItem";
+import newTask from "./newTaskWindow";
 
 (function (){
+    const projects = [{name:"Home", list:[{task:"fuck", dueDate: [1995, 10, 24], priority: "High"}, {task:"you", dueDate: "08/10/1995", priority: "Medium"},]}, 
+    {name:"This Week", list:[{task:"bitch", dueDate: [1995, 10, 24], priority: "High"}, {task:"bum", dueDate: "08/10/1995", priority: "Medium"},]}, 
+    {name:"This Month", list:[]}, 
+    {name:"Projects", list:[]}];
     const sideBar = document.getElementById("sidebar")
-
+    let currentProject = "Home"
+    function display(project){
+        let proj = projects.find(obj => {
+            return obj.name === project
+        })
+        toDoList(proj.list)
+    }
+    display(currentProject);
     const list = document.createElement("ul");
     list.classList.add("sideBarList")
 
-    const sideBarMain = ["Home", "This Week", "This Month", "Projects"];
-    sideBarMain.forEach(element => {
+  
+    projects.forEach(element => {
 
         let item = document.createElement("li");
         item.classList.add("listItem")
-        item.textContent = element;
-        item.onclick = ()=>console.log("do some shit")
+        item.textContent = element.name;
+        item.onclick = ()=>{
+            currentProject = element.name;
+            let content = document.getElementById("content")
+            while (content.firstChild) {
+                content.removeChild(content.firstChild);
+            }
+            toDoList(element.list)
+            if(element.list.length === 0){
+                let msg = document.createElement("p")
+                msg.textContent = "This list is empty"
+                content.appendChild(msg)
+            }
+        }
+
         list.appendChild(item)
 
     });
